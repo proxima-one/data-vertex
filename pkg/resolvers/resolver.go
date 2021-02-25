db4794cd9b9dfcbb3002a7700038e51a272d8173
Chase Smith <chasesmith@berkeley.edu>
2021-02-25 11:04:24 -0800

    Updating  resolvers

--- pkg/resolvers/resolver.go
+++ pkg/resolvers/resolver.go
@@ -1,7 +1,7 @@
 package resolvers
 //go:generate go run github.com/99designs/gqlgen
 import (
-	_ "github.com/proxima-one/proxima-data-vertex/pkg/models"
+	models "github.com/proxima-one/proxima-data-vertex/pkg/models"
 	gql "github.com/proxima-one/proxima-data-vertex/pkg/gql"
 	dataloader "github.com/proxima-one/proxima-data-vertex/pkg/dataloaders"
 	_ "github.com/json-iterator/go"
