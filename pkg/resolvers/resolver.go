2206875055038505a84b94ed4391edd356f89191
Chase Smith <chasesmith@berkeley.edu>
2021-02-25 13:31:42 -0800

    Updating the database

--- pkg/resolvers/resolver.go
+++ pkg/resolvers/resolver.go
@@ -4,20 +4,24 @@ import (
 	_ "github.com/proxima-one/proxima-data-vertex/pkg/models"
 	gql "github.com/proxima-one/proxima-data-vertex/pkg/gql"
 	dataloader "github.com/proxima-one/proxima-data-vertex/pkg/dataloaders"
+	proxima "github.com/proxima-one/proxima-db-client-go/pkg/database"
 	_ "github.com/json-iterator/go"
 	_ "fmt"
 )
 
+var DefaultInputs map[string]interface{} = map[string]interface{}{"prove": false, "limit": 100}
+
 // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
-type Resolver struct{
+type Resolver struct {
+	db *proxima.ProximaDatabase
 	loader *dataloader.Dataloader
 }
 
-func (r *Resolver) Query() *queryResolver {
+func (r *Resolver) Query() gql.QueryResolver {
 	return &queryResolver{r}
 }
 
-func (r *Resolver) Mutation() *mutationResolver {
+func (r *Resolver) Mutation() gql.MutationResolver {
 	return &mutationResolver{r}
 }
 
@@ -25,9 +29,10 @@ type queryResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 
 
-func NewResolver(loader *dataloader.Dataloader) (gql.Config) {
-	r := Resolver{}
+func NewResolver(loader *dataloader.Dataloader, db *proxima.ProximaDatabase) (gql.Config) {
+	r := gql.Resolver{}
 	r.loader = loader
+	r.db = db
 	return gql.Config{
 		Resolvers: &r,
 	}
